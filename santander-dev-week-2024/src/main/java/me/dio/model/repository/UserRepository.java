01e287b390d9c2148ba60c75536c10d9436cf91a
vitorlana45 <vitoresmerio00@gmail.com>
2024-02-14 23:16:18 -0300

    implementacão da camada de dominio

---
+++ santander-dev-week-2024/src/main/java/me/dio/model/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package me.dio.model.repository;
+
+
+import me.dio.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
